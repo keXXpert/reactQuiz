1e4827a82412552c9e9eb7aed893a80437453584
keXXpert <kexxpert@ya.ru>
2020-06-02 14:35:40 +0300

    added test ajax get request on firebase

--- src/components/QuizList/QuizList.jsx
+++ src/components/QuizList/QuizList.jsx
@@ -1,8 +1,14 @@
-import React from "react";
+import React, { useEffect } from "react";
 import myCSS from "./QuizList.module.css";
 import { NavLink } from 'react-router-dom';
+import * as axios from 'axios'
 
 const QuizList = (props) => {
+  useEffect(()=> {
+    axios.get('https://react-quiz-99a01.firebaseio.com/quiz.json')
+    .then(response => console.log(response))
+  }, [])
+  
   const renderQuizes = () => {
       return [1,2,3].map((quiz, index) => <li key={index} className={myCSS.li}>
           <NavLink to={'/quiz/'+ quiz}>Quiz #{quiz}</NavLink>
